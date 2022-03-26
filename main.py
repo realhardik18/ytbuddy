936d947951605e4b1cefa77852663f3bc67ec6c7
hardik singh <realhardik18@gmail.com>
2022-03-26 23:36:58 +0530

    now script can print links to all videos of a channle

--- main.py
+++ main.py
@@ -1,18 +1,13 @@
 import scrapetube
 import pandas
 
-videos = scrapetube.get_channel("UCMiY4t431lhXY4QtPZtzftQ")
-
-for video in videos:
-    print(video['videoId'])
-# work on getting ids of channle from data sheet
-'''
 df = pandas.read_excel('DataBase.xlsx')
 channel_ids = []
 urls = df['link to channle'].tolist()
 for url in urls[1:]:
     revser = url[::-1]
     first_slash = revser.index('/')
-    channel_ids.append(url[len(url)-first_slash:])
-print(channel_ids)
-'''
+    videos = scrapetube.get_channel(url[len(url)-first_slash:])
+    for video in videos:
+        print(f"https://youtube.com/watch?v={video['videoId']}")
+# work on checking validity of data by clicking random links
