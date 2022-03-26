665e02bdd40b665b28caafc0839acfdf159872c9
hardik singh <realhardik18@gmail.com>
2022-03-26 19:16:45 +0530

    working on method to get all video of channel
    have to find a way to get id of channles from data base

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 import scrapetube
 import pandas
 
+videos = scrapetube.get_channel("UCMiY4t431lhXY4QtPZtzftQ")
+
+for video in videos:
+    print(video['videoId'])
+# work on getting ids of channle from data sheet
+'''
 df = pandas.read_excel('DataBase.xlsx')
+channel_ids = []
 urls = df['link to channle'].tolist()
-for url in urls:
+for url in urls[1:]:
     revser = url[::-1]
     first_slash = revser.index('/')
-    print(scrapetube.get_channel(url[len(url)-first_slash:]))
-    # work from here
+    channel_ids.append(url[len(url)-first_slash:])
+print(channel_ids)
+'''
