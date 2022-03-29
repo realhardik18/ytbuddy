8dc5ce450385432f21b8439641d3379426b4ce26
hardik singh <realhardik18@gmail.com>
2022-03-29 14:17:20 +0530

    added extra info to data and made it into a csv file

---
+++ scripts to get data/VideoInfoGetter.py
@@ -0,0 +1,15 @@
+from pytube import YouTube
+
+
+def info(video_link):
+    information = {}
+    yt = YouTube(video_link)
+    information['video_link'] = video_link
+    information['description'] = yt
+    information['duration'] = yt.length
+    information['date'] = yt.publish_date
+    information['title'] = yt.title
+    information['views'] = yt.views
+    information['author'] = yt.author
+
+    return information
