2f81361cfe4de77e9677440195f49134e2aedf46
realhardik18 <realhardik18@gmail.com>
2022-07-09 13:43:17 +0530

    it can now showcase channle data in form of a dict

--- VideoInfoGetter.py
+++ VideoInfoGetter.py
@@ -1,7 +1,8 @@
 from pytube import YouTube
+import scrapetube
 
 
-def info(video_link):
+def infoOfVideo(video_link):
     information = {}
     yt = YouTube(video_link)
     information['video_link'] = video_link
@@ -11,5 +12,21 @@ def info(video_link):
     information['title'] = yt.title
     information['views'] = yt.views
     information['author'] = yt.author
+    information['thumbnail'] = yt.thumbnail_url
 
     return information
+
+
+def infoOfAllVids(url_to_channle):
+    all_vids = list()
+    videos = scrapetube.get_channel(
+        url_to_channle[len(url_to_channle)-url_to_channle[::-1].index('/'):])
+    for video in videos:
+        all_vids.append(f"https://youtube.com/watch?v={video['videoId']}")
+        # print(f"https://youtube.com/watch?v={video['videoId']}")
+    all_data = []
+    for video in all_vids:
+        all_data.append(infoOfVideo(video))
+    return all_data
+# print(info(all_vids[0]))
+# work on site aspect now
--- app.py
+++ app.py
@@ -1,12 +1,19 @@
 from flask import Flask, render_template, request
 from threading import Thread
+from VideoInfoGetter import infoOfAllVids
 
 app = Flask('')
 
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template("index.html")
+
+
+@app.route('/result', methods=['POST'])
+def post():
+    user_input = request.form['text']
+    return render_template('result.html', data=infoOfAllVids(user_input))
 
 
 app.run()
