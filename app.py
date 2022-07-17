fe7b3e8d75e21a74972cd8a055a338dbabef0382
realhardik18 <realhardik18@gmail.com>
2022-07-18 00:11:10 +0530

    table alginment try?!?!

--- app.py
+++ app.py
@@ -14,7 +14,9 @@ def home():
 def post():
     user_input = request.form['text']
     # return render_template('result.html', data=infoOfAllVids(user_input))
-    return render_template('all.html', data=ytbuddy.infoOfAllVids(user_input).value)
+    with open('sampledata.txt') as file:
+        test_data = file.read()
+    return render_template('all.html', data=test_data)
 
 
 app.run(debug=True)
--- test.py
+++ test.py
@@ -3,4 +3,6 @@ import pydaisi as pyd
 ytbuddy = pyd.Daisi("realhardik18/ytbuddy")
 
 # print(ytbuddy.test().value)
-print(ytbuddy.endpoints.keys())
+# print(ytbuddy.endpoints.keys())
+print(ytbuddy.infoOfAllVids(
+    'https://www.youtube.com/channel/UCJBtCUf_GEvlYeOwUSnCC0Q').value)
