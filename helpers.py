d47c204cd4a7de1f08b4623d5e3e4cd5c60e8d7a
realhardik18 <realhardik18@gmail.com>
2022-07-23 17:24:11 +0530

    Update helpers.py

--- helpers.py
+++ helpers.py
@@ -1,2 +1,16 @@
+from PIL import Image
+from io import BytesIO
+import requests
+
+
 def test():
     return 'hello world'
+
+
+def max_colors(thumbnail_URL):
+    response = requests.get(thumbnail_URL)
+    img = Image.open(BytesIO(response.content))
+    return img.getcolors()
+
+
+print(max_colors('https://i.ytimg.com/vi/troEfX2xU7g/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAriuyAF6KnU30xGkqYCCWybgAMzA'))
