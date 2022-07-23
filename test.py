7d594c455189f28d3806f6bebdf37dab6d37a2fc
realhardik18 <realhardik18@gmail.com>
2022-07-23 17:02:40 +0530

    revamping whole project

--- test.py
+++
@@ -1,8 +0,0 @@
-import pydaisi as pyd
-
-ytbuddy = pyd.Daisi("realhardik18/ytbuddy")
-
-# print(ytbuddy.test().value)
-# print(ytbuddy.endpoints.keys())
-print(ytbuddy.infoOfAllVids(
-    'https://www.youtube.com/channel/UCJBtCUf_GEvlYeOwUSnCC0Q').value)
