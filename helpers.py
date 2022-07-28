0482b05522a5bd66979307a2bac6fddfd5bc0058
realhardik18 <realhardik18@gmail.com>
2022-07-28 13:45:55 +0530

    Update helpers.py

--- helpers.py
+++ helpers.py
@@ -1,4 +1,5 @@
 from pytube import YouTube
+import youtube_dl
 
 
 def test():
@@ -18,3 +19,19 @@ def stats(video_link):
     information['title'] = yt.title
     information['views'] = yt.views
     return information
+
+
+def download_as_mp3(link_to_video):
+
+    ydl_opts = {
+        'format': 'bestaudio/best',
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '320',
+        }],
+    }
+    print('starting download')
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([link_to_video])
+    return 'finished!'
